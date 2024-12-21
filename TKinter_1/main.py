ea82c6ed88f064e05a5ae4b9933c9e9e9be766b0
marvid777 <59563463+marvid777@users.noreply.github.com>
2024-12-21 16:32:47 +0100

    Update main.py

--- TKinter_1/main.py
+++ TKinter_1/main.py
@@ -3,12 +3,21 @@ import tkinter as tk
 # tkinter._test()
 
 root = tk.Tk()
+root.title("Das ist das Hauptfenster")
+root.geometry("800x400")
+root.minsize(width=250, height=250)
+root.maxsize(width=600, height=600)
+root.resizable(width=False, height=True)
 
-label1 = tk.Label(root, text="Hallo Martin")
-label1.pack()
 
-label2 = tk.Label(root, text="Hallo Marie")
-label2.pack()
+label1 = tk.Label(root, text="Hallo Martin", bg="green")
+label1.pack(side="top", expand=True,  fill="y")
+
+label2 = tk.Label(root, text="Hallo Marie", bg="red")
+label2.pack(side="top", expand=True,  fill="both")
 
 
 root.mainloop()
+
+# print("Textausgabe")
+
