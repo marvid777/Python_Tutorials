7d580d8b89fb20d906df9140002f74b842a84c5f
marvid777 <59563463+marvid777@users.noreply.github.com>
2024-12-21 15:53:21 +0100

    Update main.py

--- TKinter_1/main.py
+++ TKinter_1/main.py
@@ -1,1 +1,14 @@
+import tkinter as tk
 
+# tkinter._test()
+
+root = tk.Tk()
+
+label1 = tk.Label(root, text="Hallo Martin")
+label1.pack()
+
+label2 = tk.Label(root, text="Hallo Marie")
+label2.pack()
+
+
+root.mainloop()
