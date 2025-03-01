024e0ffd645b75ea964a9c8440d6d29be5145248
marvid777 <59563463+marvid777@users.noreply.github.com>
2025-03-01 19:41:01 +0100

    Add files via upload

---
+++ 30_PyQt/PyQt5 Daily Task Planning/main.py
@@ -0,0 +1,16 @@
+from PyQt5.QtWidgets import QWidget, QApplication, QListWidgetItem, QMessageBox
+from PyQt5.uic import loadUi
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+        loadUi("main.ui", self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
